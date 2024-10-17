fa010b93039b2db061f99c8623abf12ab4ad3bf5
mohiral <kumawatmohit2505@gmail.com>
2024-10-17 19:45:21 +0530

    changes

--- server.js
+++ server.js
@@ -16,12 +16,22 @@ import seller2ProductRoutes from './routes/cafe/seller2/Seller2_products.js';
 import Kiranaseller1CategoryRoutes from './routes/Kirana/seller1/Kirana_Seller1_categories.js';
 import Kiranaseller1ProductRoutes from './routes/Kirana/seller1/Kirana_Seller1_products.js';
 
+// Import Services seller1 routses
+import Services_Center_seller1CategoryRoutes from './routes/Services_Center/seller1/Services_Center_Seller1_categories.js';
+import Services_Center_seller1ProductRoutes from './routes/Services_Center/seller1/Services_Center_Seller1_products.js';
+
+// Import Services seller1 routses
+import Services_Center_Seller1_Service_categories from './routes/Services_Center/seller1/Services_Center_Seller1_Service_categories.js';
+import Services_Center_Seller1_Services from './routes/Services_Center/seller1/Services_Center_Seller1_Services.js';
+
 // Import user and cart routes
 import userRoutes from './user/routes/userRoutes.js';
 import cartRoutes from './user/routes/cartRoutes.js';
 
 // Import order routes
 import orderRoutes from './user/routes/orderRoutes.js';
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -50,6 +60,16 @@ app.use('/seller2_products', seller2ProductRoutes);
 // Routes for Kirana seller1
 app.use('/Kiranaseller1_categories', Kiranaseller1CategoryRoutes);
 app.use('/Kiranaseller1_products', Kiranaseller1ProductRoutes);
+
+// Routes for Services seller1
+
+app.use('/Services_Center_seller1_categories', Services_Center_seller1CategoryRoutes);
+app.use('/Services_Center_seller1_products', Services_Center_seller1ProductRoutes);
+
+// Routes for Services seller1
+
+app.use('/Services_Center_Seller1_Service_categories', Services_Center_Seller1_Service_categories);
+app.use('/Services_Center_Seller1_Services', Services_Center_Seller1_Services);
 
 // User and cart routes
 app.use('/cart', cartRoutes);
